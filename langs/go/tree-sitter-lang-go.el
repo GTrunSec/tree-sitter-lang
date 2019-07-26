b475d24a86346a75ff28e4d03367969210579e06
gtrunsec <gtrun@outlook.com>
2019-07-25 22:43:30 -0700

    lang-go

--- langs/go/tree-sitter-lang-go.el
+++ langs/go/tree-sitter-lang-go.el
@@ -1,7 +1,7 @@
 ;;; tree-sitter.el --- Emacs bindings to the tree-sitter parser for python  -*- lexical-binding: t; -*-
 
 ;;;###autoload
-(autoload 'tree-sitter-lang-python "tree-sitter-lang-go-module"
+(autoload 'tree-sitter-lang-go "tree-sitter-lang-go-module"
   "Return the tree-sitter language object for python.
 
 (fn)")
